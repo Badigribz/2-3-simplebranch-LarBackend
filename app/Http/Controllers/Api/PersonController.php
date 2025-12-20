b81ec4ed45eb604d09d20d7a416d76c0b2a18b98
Gribz <gribzbadi@gmail.com>
2025-12-20 23:08:35 +0300

    addding API personController

---
+++ app/Http/Controllers/Api/PersonController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    // GET all people
+    public function index()
+    {
+        return Person::all();
+    }
+
+    // POST add person
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'parent_id' => 'nullable|exists:people,id',
+        ]);
+
+        return Person::create($data);
+    }
+
+    // PATCH rename
+    public function update(Request $request, Person $person)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $person->update([
+            'name' => $request->name,
+        ]);
+
+        return $person;
+    }
+}
