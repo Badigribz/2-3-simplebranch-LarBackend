dd6472adca5262497ff4e77c7f0e8abfee1ecb34
Gribz <gribzbadi@gmail.com>
2025-12-20 22:34:59 +0300

    Moded people schema

---
+++ app/Models/Person.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    protected $fillable = [
+        'name',
+        'parent_id',
+        'bio',
+        'photo_path',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(Person::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Person::class, 'parent_id');
+    }
+}
