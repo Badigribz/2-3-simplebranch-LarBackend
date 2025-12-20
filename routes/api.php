d20e1a1026ad3f0d8e055b579681dc3a9421636e
Gribz <gribzbadi@gmail.com>
2025-12-20 23:24:45 +0300

    added api.php

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PersonController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/people', [PersonController::class, 'index']);
+Route::post('/people', [PersonController::class, 'store']);
+Route::patch('/people/{person}', [PersonController::class, 'update']);
