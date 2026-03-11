e59bce205d51260f56aca9eca973e96ab0a14938
Gribz <gribzbadi@gmail.com>
2026-03-11 22:40:36 +0300

    mod

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::delete('/people/{person}', [PersonController::class, 'destroy']);
 Route::get('/people/{person}', [PersonController::class, 'show']);
 Route::post('/people/{person}/photo', [PersonController::class, 'uploadPhoto']);
 
-// ── Gallery photo routes (NEW) ──
+// ── Gallery photo routes ──
 Route::get('/people/{person}/photos', [PhotoController::class, 'index']);
 Route::post('/people/{person}/photos', [PhotoController::class, 'store']);
 Route::delete('/photos/{photo}', [PhotoController::class, 'destroy']);
